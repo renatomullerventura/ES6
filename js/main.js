9433e79a37a45caad54e111ed2e8c23b61f58d76
Renato Muller Ventura <re.infotec@gmail.com>
2016-07-28 17:55:07 +0000

    2° aula de ES6, arrow functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Client = function Client(name, email) {
-    _classCallCheck(this, Client);
-
-    this.name = name;
-    this.email = email;
+/*
+var bob =   {
+    _name: "Bob",
+    _friends: ["Guilherme", "Camila", "Murilo"],
+    printFriends: function printFriends(){
+        var self = this;
+        this._friends.forEach(function(f){
+            return console.log(self._name + ' knows ' + f);
+        });
+    }
 };
+
+bob.printFriends();
+
+
+var color = () => console.log("red");
+
+color();
+*/
+
+var bob = {
+    _name: "Bob",
+    _friends: ["Guilherme", "Camila", "Murilo"],
+    printFriends: function printFriends() {
+        var _this = this;
+
+        this._friends.forEach(function (f) {
+            return console.log(_this._name + " knows " + f);
+        });
+    }
+};
+
+bob.printFriends();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
-class Client{
-    constructor(name, email){
-        this.name   =   name;
-        this.email  =   email;
+/*
+var bob =   {
+    _name: "Bob",
+    _friends: ["Guilherme", "Camila", "Murilo"],
+    printFriends: function printFriends(){
+        var self = this;
+        this._friends.forEach(function(f){
+            return console.log(self._name + ' knows ' + f);
+        });
     }
-}
+};
+
+bob.printFriends();
+
+
+var color = () => console.log("red");
+
+color();
+*/
+
+
+var bob =   {
+    _name: "Bob",
+    _friends: ["Guilherme", "Camila", "Murilo"],
+    printFriends: function printFriends(){
+        this._friends.forEach(f => console.log(this._name + " knows " + f));
+    }
+};
+
+bob.printFriends();
