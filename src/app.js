3ef24ce4be64216781763286ee467a18400b38f8
Renato Muller Ventura <re.infotec@gmail.com>
2016-08-01 11:49:24 +0000

    7ª aula, herança.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
-var list = ['test', 34, {name:'Guilherme'}];
-
-for (var i in list) {
-    //console.log(list[i]);
+class People{
+    constructor(name, email, phone){
+        this.name   =   name;
+        this.email  =   email;
+        this.phone  =   phone;
+    }
+    toString(){
+        return  `Name: ${this.name}
+        Email: ${this.email}
+        Phone: ${this.phone}
+        `;
+    }
 }
 
-for(let i of list){
-    console.log(i);
+class Client extends People{
+    constructor(id, name, email, phone){
+        super(name, email, phone);
+        this.id =   id;
+    };
+    toString(){
+        return `
+        id: ${this.id}
+        ${super.toString()}
+        `;
+    }
 }
+
+let test    =   new Client(1, "Renato", "re.infotec@gmail.com", "1234567");
+console.log(test.toString());
